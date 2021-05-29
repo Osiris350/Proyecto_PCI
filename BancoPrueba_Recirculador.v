10e31b43f9eea4f36b0269d86094b198be846f99
jdgg98 <44977262+jdgg98@users.noreply.github.com>
2021-05-28 20:04:23 -0600

    Update BancoPrueba_Recirculador.v

--- BancoPrueba_Recirculador.v
+++ BancoPrueba_Recirculador.v
@@ -1,129 +1,187 @@
 /** 
-* Se incluye la directiva de compilar timescale para indicar el valor
-* de cada unidad de tiempo.
-* Unidad: 1ns
-* Precision: 1ns
+* Se incluye el contenido de los archivos Recirculador_sintetizado.v 
+* y Probador_Probador.v, los cuales contienen el modulo recirculador 
+* y su probador
 **/
-`timescale 1ns/1ns
+`include "Recirculador.v"
+`include "Recirculador_sintetizado.v"
+`include "Probador_Recirculador.v"
+`include "cmos_cells.v"
 
 /**
-* Modulo de prueba en el que se aplicaran las señales de entrada
-* y se observaran las respuestas del modulo Recirculador
+* Modulo testbench para el recirculador diseñado
 **/
-
-module Probador_Recirculador(
-	output reg clk,
-	output reg [7:0]dataIn0,
-	output reg[ 7:0]dataIn1,
-	output reg [7:0]dataIn2,
-	output reg [7:0]dataIn3,
-	output reg validIn0,
-    output reg validIn1,
-    output reg validIn2,
-    output reg validIn3,
-	output reg selector_IDLE,
-	input [7:0]dataOut0,
-    input [7:0]dataOut1,
-	input [7:0]dataOut2,
-    input [7:0]dataOut3,
-	input [7:0]dataOut4,
-    input [7:0]dataOut5,
-	input [7:0]dataOut6,
-    input [7:0]dataOut7,
-	input validOut0,
-	input validOut1,
-	input validOut2,
-	input validOut3,
-	input validOut4,
-	input validOut5,
-	input validOut6,
-	input validOut7
-	);
-
-	reg clk4, clk2;
-	/**
-	* Los siguientes parameters serviran como entradas
-	* de prueba para el recirculador
-	**/
-
-	parameter FF = 8'hFF;
-	parameter BB = 8'hBB;
-	parameter EE = 8'hEE;
-	parameter AA = 8'hAA;
-	parameter DD = 8'hDD;
-	parameter x99 = 8'h99;
-	parameter CC = 8'hCC;
-	parameter x88 = 8'h88;
-	parameter x77 = 8'h77;
-	parameter X = 8'hx;
-
-	initial 
-		begin
-			{clk4, clk2, selector_IDLE, validIn0, validIn1, validIn2, validIn3} = 0;
-			clk = 1;
-		end
-
-	always #2 clk = ~clk;
-	
-	always@(posedge clk) clk2 = ~clk2;
-
-	always@(posedge clk2) clk4 = ~clk4;
+module Testbench();
 
 	/** 
-	* Se dispone de un initial block para estimular todas
-	* las señales especificas en una sola prueba
+	* Se declaran los wires que van a conectar cada una de las entradas
+	* y salidas entre el modulo Recirculador_sintetizado y su probador
 	**/
-	initial
-		begin
+	wire clk;
+	wire [7:0]dataIn0;
+	wire [7:0]dataIn1;
+	wire [7:0]dataIn2;
+	wire [7:0]dataIn3;
+	wire validIn0;
+    wire validIn1;
+    wire validIn2;
+    wire validIn3;
+	wire selector_IDLE;
+    wire [7:0]dataOut0_cond;
+    wire [7:0]dataOut1_cond;
+	wire [7:0]dataOut2_cond;
+    wire [7:0]dataOut3_cond;
+	wire [7:0]dataOut4_cond;
+    wire [7:0]dataOut5_cond;
+	wire [7:0]dataOut6_cond;
+    wire [7:0]dataOut7_cond;
+	wire validOut0_cond;
+	wire validOut1_cond;
+	wire validOut2_cond;
+	wire validOut3_cond;
+	wire validOut4_cond;
+	wire validOut5_cond;
+	wire validOut6_cond;
+	wire validOut7_cond;
+    wire [7:0]dataOut0_synth;
+    wire [7:0]dataOut1_synth;
+	wire [7:0]dataOut2_synth;
+    wire [7:0]dataOut3_synth;
+	wire [7:0]dataOut4_synth;
+    wire [7:0]dataOut5_synth;
+	wire [7:0]dataOut6_synth;
+    wire [7:0]dataOut7_synth;
+	wire validOut0_synth;
+	wire validOut1_synth;
+	wire validOut2_synth;
+	wire validOut3_synth;
+	wire validOut4_synth;
+	wire validOut5_synth;
+	wire validOut6_synth;
+	wire validOut7_synth;
 
-			/**
-			* Se define el nombre el archivo .vcd en cual se
-			* guardaran todos los valores de las nets y regs que
-			* cambian en la simulacion
-			**/
+	/** 
+	* Se instancian los modulos del Recirculador_sintetizado y su probador,
+	* y se conectan sus entradas y salidas a los correspondientes wires
+	**/
 
-			$dumpfile("Recirculador.vcd");
-			$dumpvars; // Especificacion para que se guarden todas las variables en el archivo .vcd
+	Recirculador recirculador_cond(/*AUTOINST*/
+				       // Outputs
+				       .dataOut0_cond	(dataOut0_cond[7:0]),
+				       .dataOut1_cond	(dataOut1_cond[7:0]),
+				       .dataOut2_cond	(dataOut2_cond[7:0]),
+				       .dataOut3_cond	(dataOut3_cond[7:0]),
+				       .dataOut4_cond	(dataOut4_cond[7:0]),
+				       .dataOut5_cond	(dataOut5_cond[7:0]),
+				       .dataOut6_cond	(dataOut6_cond[7:0]),
+				       .dataOut7_cond	(dataOut7_cond[7:0]),
+				       .validOut0_cond	(validOut0_cond),
+				       .validOut1_cond	(validOut1_cond),
+				       .validOut2_cond	(validOut2_cond),
+				       .validOut3_cond	(validOut3_cond),
+				       .validOut4_cond	(validOut4_cond),
+				       .validOut5_cond	(validOut5_cond),
+				       .validOut6_cond	(validOut6_cond),
+				       .validOut7_cond	(validOut7_cond),
+				       // Inputs
+				       .clk		(clk),
+				       .dataIn0		(dataIn0[7:0]),
+				       .dataIn1		(dataIn1[7:0]),
+				       .dataIn2		(dataIn2[7:0]),
+				       .dataIn3		(dataIn3[7:0]),
+				       .validIn0	(validIn0),
+				       .validIn1	(validIn1),
+				       .validIn2	(validIn2),
+				       .validIn3	(validIn3),
+				       .selector_IDLE	(selector_IDLE));
 
-			#2
-			dataIn0 = FF;
-			dataIn1 = EE;
-			dataIn2 = DD;
-			dataIn3 = CC;
+	Probador_Recirculador signals_generator_cond(
+						     // Outputs
+						     .clk_1			(clk),
+						     .dataIn0		(dataIn0[7:0]),
+						     .dataIn1		(dataIn1[7:0]),
+						     .dataIn2		(dataIn2[7:0]),
+						     .dataIn3		(dataIn3[7:0]),
+						     .validIn0		(validIn0),
+						     .validIn1		(validIn1),
+						     .validIn2		(validIn2),
+						     .validIn3		(validIn3),
+						     .selector_IDLE	(selector_IDLE),
+						     // Inputs
+						     .dataOut0		(dataOut0_cond[7:0]),
+						     .dataOut1		(dataOut1_cond[7:0]),
+						     .dataOut2		(dataOut2_cond[7:0]),
+						     .dataOut3		(dataOut3_cond[7:0]),
+						     .dataOut4		(dataOut4_cond[7:0]),
+						     .dataOut5		(dataOut5_cond[7:0]),
+						     .dataOut6		(dataOut6_cond[7:0]),
+						     .dataOut7		(dataOut7_cond[7:0]),
+						     .validOut0		(validOut0_cond),
+						     .validOut1		(validOut1_cond),
+						     .validOut2		(validOut2_cond),
+						     .validOut3		(validOut3_cond),
+						     .validOut4		(validOut4_cond),
+						     .validOut5		(validOut5_cond),
+						     .validOut6		(validOut6_cond),
+						     .validOut7		(validOut7_cond));
 
-			validIn0 = 1;
-			validIn1 = 1;
-			validIn2 = 1;
-			validIn3 = 1;
+	Recirculador_sintetizado recirculador_synth(/*AUTOINST*/
+						    // Outputs
+						    .dataOut0_synth	(dataOut0_synth[7:0]),
+						    .dataOut1_synth	(dataOut1_synth[7:0]),
+						    .dataOut2_synth	(dataOut2_synth[7:0]),
+						    .dataOut3_synth	(dataOut3_synth[7:0]),
+						    .dataOut4_synth	(dataOut4_synth[7:0]),
+						    .dataOut5_synth	(dataOut5_synth[7:0]),
+						    .dataOut6_synth	(dataOut6_synth[7:0]),
+						    .dataOut7_synth	(dataOut7_synth[7:0]),
+						    .validOut0_synth	(validOut0_synth),
+						    .validOut1_synth	(validOut1_synth),
+						    .validOut2_synth	(validOut2_synth),
+						    .validOut3_synth	(validOut3_synth),
+						    .validOut4_synth	(validOut4_synth),
+						    .validOut5_synth	(validOut5_synth),
+						    .validOut6_synth	(validOut6_synth),
+						    .validOut7_synth	(validOut7_synth),
+						    // Inputs
+						    .clk		(clk),
+						    .dataIn0		(dataIn0[7:0]),
+						    .dataIn1		(dataIn1[7:0]),
+						    .dataIn2		(dataIn2[7:0]),
+						    .dataIn3		(dataIn3[7:0]),
+						    .selector_IDLE	(selector_IDLE),
+						    .validIn0		(validIn0),
+						    .validIn1		(validIn1),
+						    .validIn2		(validIn2),
+						    .validIn3		(validIn3));
 
-			#4
-			selector_IDLE = 1;
-
-			#4
-			dataIn0 = BB;
-			dataIn1 = AA;
-			dataIn2 = x99;
-			dataIn3 = x88;
-
-			#8
-			dataIn0 = X;
-			dataIn1 = X;
-			dataIn2 = x77;
-			dataIn3 = X;
-
-			validIn0 = 0;
-			validIn1 = 0;
-			validIn3 = 0;
-
-			#8
-			dataIn2 = X;
-			validIn2 = 0;
-
-			/** 
-			* Se dan 4 unidades de tiempo mas para poder
-			* observar los resultados del ultima cambio
-			* y se termina la simulacion con el keyword finish
-			**/
-			#4 $finish;
-		end
+    Probador_Recirculador signals_generator_synth(
+						  // Outputs
+						  .clk_1		(clk),
+						  .dataIn0		(dataIn0[7:0]),
+						  .dataIn1		(dataIn1[7:0]),
+						  .dataIn2		(dataIn2[7:0]),
+						  .dataIn3		(dataIn3[7:0]),
+						  .validIn0		(validIn0),
+						  .validIn1		(validIn1),
+						  .validIn2		(validIn2),
+						  .validIn3		(validIn3),
+						  .selector_IDLE	(selector_IDLE),
+						  // Inputs
+						  .dataOut0		(dataOut0_synth[7:0]),
+						  .dataOut1		(dataOut1_synth[7:0]),
+						  .dataOut2		(dataOut2_synth[7:0]),
+						  .dataOut3		(dataOut3_synth[7:0]),
+						  .dataOut4		(dataOut4_synth[7:0]),
+						  .dataOut5		(dataOut5_synth[7:0]),
+						  .dataOut6		(dataOut6_synth[7:0]),
+						  .dataOut7		(dataOut7_synth[7:0]),
+						  .validOut0		(validOut0_synth),
+						  .validOut1		(validOut1_synth),
+						  .validOut2		(validOut2_synth),
+						  .validOut3		(validOut3_synth),
+						  .validOut4		(validOut4_synth),
+						  .validOut5		(validOut5_synth),
+						  .validOut6		(validOut6_synth),
+						  .validOut7		(validOut7_synth));
 endmodule
